aa7e448cfcabda050e32b5a1c0cbe1c1ad623179
WorldShredder <183172489+WorldShredder@users.noreply.github.com>
2026-07-26 08:46:46 +0000

    refactor(sesh): increase fd search depth for sesh find

--- src/library/400_install_sesh/extras/shellrc/400_sesh_bindings.bash
+++ src/library/400_install_sesh/extras/shellrc/400_sesh_bindings.bash
@@ -19,7 +19,7 @@ __sesh_search() {
                         --bind 'ctrl-t:change-prompt(  )+reload(sesh list -t --icons)' \
                         --bind 'ctrl-g:change-prompt(  )+reload(sesh list -c --icons)' \
                         --bind 'ctrl-x:change-prompt(  )+reload(sesh list -z --icons)' \
-                        --bind 'ctrl-f:change-prompt(  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
+                        --bind 'ctrl-f:change-prompt(  )+reload(fd -H -d 3 -t d -E .Trash . ~)' \
                         --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(  )+reload(sesh list --icons)' \
                         --preview-window 'right:55%' \
                         --preview 'sesh preview {}'

750f43272afd10f80735f80d1fd699fc68c000d3
WorldShredder <183172489+WorldShredder@users.noreply.github.com>
2026-07-26 07:47:27 +0000

    fix: wget required by some library conditions

--- src/modules/010_bootstrap/010_install_required_packages.sh
+++ src/modules/010_bootstrap/010_install_required_packages.sh
@@ -7,8 +7,8 @@ trap 'exit $?' ERR
 
 declare -a required
 case "$WSE__DISTRIB" in
-    debian) required+=(curl git tar xz-utils unzip jq) ;;
-    fedora) required+=(curl git tar xz unzip jq) ;;
+    debian) required+=(curl wget git tar xz-utils unzip jq) ;;
+    fedora) required+=(curl wget git tar xz unzip jq) ;;
     *) exit 1 ;;
 esac
 
